fa88e7ee8f3e07dc1a06d25c7e7f1494896d1f0a
Sam Burgos <sam.burgos1089@gmail.com>
2018-08-09 17:04:15 -0600

    updated applet with working prototype function

--- asl@santiago.com/files/asl@santiago.com/applet.js
+++ asl@santiago.com/files/asl@santiago.com/applet.js
@@ -2,29 +2,57 @@ const Applet = imports.ui.applet;
 const Util = imports.misc.util;
 const GLib = imports.gi.GLib;
 const Tooltips = imports.ui.tooltips;
+const St = imports.gi.St;
+const PopupMenu = imports.ui.popupMenu;
 const UUID = "asl@santiago.com";
 
 
 const APPLET_PATH = global.userdatadir + "/applets/" + UUID;
 const ICON = APPLET_PATH + "/icon.png";
+const web = "https://www.handspeak.com/";
 
-function MyApplet(metadata, orientation, panel_height, instance_id) {
-    this._init(metadata, orientation, panel_height, instance_id);
+function MyApplet(metadata, orientation, panelHeight, instance_id) {
+    this._init(metadata, orientation, panelHeight, instance_id);
 }
 
 MyApplet.prototype = {
     __proto__: Applet.IconApplet.prototype,
 
-    _init: function(orientation, paneHeight, instance_id) {
-        Applet.IconApplet.prototype._init.call(this, orientation, panelHeight, instance_id);
+    _init: function(orientation, panelHeight, instanceId) {
+        Applet.IconApplet.prototype._init.call(this, orientation, panelHeight, instanceId);
 
-        this.set_applet_icon_path(ICON);
-        this.set_applet_tooltip(_("American Sign Language"));
+        try
+        {
+
+
+          this.set_applet_icon_path(ICON);
+          //this.set_applet_tooltip(_("American Sign Language"));
+
+        	//Initializing options on the Popup menu
+        	this.menuManager = new PopupMenu.PopupMenuManager(this);
+			    this.menu = new Applet.AppletPopupMenu(this, orientation);
+			    this.menuManager.addMenu(this.menu);
+
+			    //Adding items to the Popup Menu
+			    let item = new PopupMenu.PopupIconMenuItem("Abecedario", "send-to", St.IconType.SYMBOLIC);
+			    this.menu.addMenuItem(item);
+
+			    item = new PopupMenu.PopupIconMenuItem("Colores", "send-to", St.IconType.SYMBOLIC);
+			    this.menu.addMenuItem(item);
+
+        }
+
+        catch(e)
+        {
+        	global.logError(e);
+        }
+
     },
 
-    /*on_applet_clicked: function() {
-        Util.spawn('xkill');
-    }*/
+    on_applet_clicked: function(event) {
+        //this.menu.toggle();
+        Util.spawnCommandLine("xdg-open https://www.youtube.com/");
+    }
 };
 
 function main(metadata, orientation, panelHeight, instance_id) {

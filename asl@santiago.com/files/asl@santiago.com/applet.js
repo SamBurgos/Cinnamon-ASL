c4c4149b5cfa6ddcc28b856c76ca4b50e584733a
Sam Burgos <sam.burgos1089@gmail.com>
2018-08-10 16:54:10 -0600

    updated prototype function applet

--- asl@santiago.com/files/asl@santiago.com/applet.js
+++ asl@santiago.com/files/asl@santiago.com/applet.js
@@ -35,44 +35,47 @@ MyApplet.prototype = {
     populate_menu: function() {
         //Adding items to the Popup Menu
         let menuitem = new PopupMenu.PopupIconMenuItem(
-            "Abecedario",
-            "send-to",
+            "Handspeak website",
+            "system-search",
             St.IconType.SYMBOLIC
         );
         menuitem.connect("activate", () => {
-            Main.notify("Abecedario", "Abecedario message body.");
+            //Main.notify("Abecedario", "Abecedario message body.");
+            Util.spawn_async(["xdg-open", website], null);
         });
         this.menu.addMenuItem(menuitem);
 
-        /*menuitem = new PopupMenu.PopupIconMenuItem("Colores", "send-to", St.IconType.SYMBOLIC);
-        menuitem.connect('activate', () => {
-            Util.spawn_async(["xdg-open", "firefox", website], null);
-        });
-        this.menu.addMenuItem(menuitem);*/
-
         //Submenu 1: Abecedario ASL
-        let subMenu1 = new PopupMenu.PopupSubMenuMenuItem("Troubleshoot");
+        let subMenu1 = new PopupMenu.PopupSubMenuMenuItem("Abecedario");
         // Add sub-menu to applet menu
         this.menu.addMenuItem(subMenu1);
 
         // First sub-menu item.
-        subMenu1.menu.addAction("Restart Cinnamon", () => {
-            global.reexec_self();
+        subMenu1.menu.addAction("Do something", () => {
+            //global.reexec_self();
+            Main.notify("Title", "Letra A en ASL");
         });
 
         // Second sub-menu item.
-        subMenu1.menu.addAction("Do whatever", () => {
-            Main.notify("Title", "Message body.");
+        subMenu1.menu.addAction("Do something else", () => {
+            Main.notify("Title", "Letra B en ASL");
         });
         this.menu.addMenuItem(menuitem);
 
         // Third sub-menu item
         menuitem = new PopupMenu.PopupIconMenuItem("Colores", "send-to", St.IconType.SYMBOLIC);
         subMenu1.menu.addMenuItem(menuitem);
+
+
+        // Fourth sub-menu item.
+        subMenu1.menu.addAction("Do something else", () => {
+            Main.notify("Title", "Letra B en ASL");
+        });
+        this.menu.addMenuItem(menuitem);
         //Fin de submenu 1
 
         //Submenu 2: Colores ASL
-        let subMenu2 = new PopupMenu.PopupSubMenuMenuItem("Troubleshoot");
+        let subMenu2 = new PopupMenu.PopupSubMenuMenuItem("Colores");
         // Add sub-menu to applet menu
         this.menu.addMenuItem(subMenu2);
 
@@ -92,13 +95,38 @@ MyApplet.prototype = {
         subMenu2.menu.addMenuItem(menuitem);
         //Fin de submenu 2
 
+        //Submenu 3: Abecedario ASL
+        let subMenu3 = new PopupMenu.PopupSubMenuMenuItem("Familia y miembros");
+        // Add sub-menu to applet menu
+        this.menu.addMenuItem(subMenu3);
+
+        // First sub-menu item.
+        subMenu3.menu.addAction("Restart Cinnamon", () => {
+            global.reexec_self();
+        });
+
+        // Second sub-menu item.
+        subMenu3.menu.addAction("Do whatever", () => {
+            Main.notify("Title", "Message body.");
+        });
+        this.menu.addMenuItem(menuitem);
+
+        // Third sub-menu item
+        menuitem = new PopupMenu.PopupIconMenuItem("Colores", "send-to", St.IconType.SYMBOLIC);
+        subMenu3.menu.addMenuItem(menuitem);
+        //Fin de submenu 3
+
+        menuitem = new PopupMenu.PopupIconMenuItem("Youtube", "video-x-generic", St.IconType.SYMBOLIC);
+        menuitem.connect('activate', () => {
+            Util.spawn_async(["xdg-open", "https://www.youtube.com"], null);
+        });
+        this.menu.addMenuItem(menuitem);
 
 
     },
 
     on_applet_clicked: function(event) {
         this.menu.toggle();
-        //Util.spawnCommandLine("xdg-open " + web);
     }
 };
 
